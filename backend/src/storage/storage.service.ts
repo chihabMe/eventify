f6dbe64660b259fb3ad97b1f8ad414a96adc78cc
chihabMe <chihab.mg.me@gmail.com>
2025-05-03 13:17:49 +0100

    added storage service

---
+++ backend/src/storage/storage.service.ts
@@ -0,0 +1,33 @@
+import { Storage } from '@google-cloud/storage';
+import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class StorageService {
+  private storage: Storage;
+  private bucketName = process.env.GOOGLE_CLOUD_STORAGE_BUCKET_NAME;
+  constructor() {
+    this.storage = new Storage({
+      projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+      keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    });
+  }
+
+  async uploadFile(file: Express.Multer.File): Promise<string> {
+    const bucket = this.storage.bucket(this.bucketName);
+    const filename = `${uuidv4()}-${file.originalname}`;
+    const blob = bucket.file(filename);
+    const blobStream = blob.createWriteStream({
+      resumable: true,
+      contentType: file.mimetype,
+    });
+    return new Promise<string>((resolve, reject) => {
+      blobStream.on('error', (err) => reject(err));
+      blobStream.on('finish', () => {
+        const publicUrl = `https://storage.googleapis.com/${this.bucketName}/${filename}`;
+        resolve(publicUrl);
+      });
+      blobStream.end(file.buffer);
+    });
+  }
+}
