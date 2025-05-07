759c5461766d70cd3828d57907e127c34bf0c72d
chihabMe <chihab.mg.me@gmail.com>
2025-05-07 01:33:24 +0100

    removed the .vscode folder

--- .vscode/src/components/SectionContainer.tsx
+++
@@ -1,9 +0,0 @@
-const SectionContainer = ({ children, className = '' }) => {
-  return (
-    <section className={`py-12 px-4 container mx-auto ${className}`}>
-      {children}
-    </section>
-  );
-};
-
-export default SectionContainer;
--- .vscode/src/constants.ts
+++
@@ -1,2 +0,0 @@
-//export const API = process.env.API_URl
-export const API = 'http://localhost:8000/api';
