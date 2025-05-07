0d8fe9e459592e8754f498cddcc451762b4ca4d5
chihabMe <chihab.mg.me@gmail.com>
2025-05-07 14:03:51 +0100

    improved the docker files/compose and optimzed it for production

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -20,11 +20,14 @@ async function bootstrap() {
   app.use(helmet());
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use(cookieParser());
+  if(process.env.CLIENT_URL){
+
   app.enableCors({
     origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
+  }
   app.useGlobalPipes(new ZodValidationPipe());
   app.useGlobalFilters(new CustomBadRequestExceptionFilter());
 
