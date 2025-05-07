e6847e2f95e1261048418adb3c7373b015bfd5cb
chihabMe <chihab.mg.me@gmail.com>
2025-05-07 14:17:21 +0100

    added VITE_ prefix to the env vars

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -1,2 +1,2 @@
 //export const API = process.env.API_URl
-export const API = import.meta.env.API_URL ?? 'http://localhost:8000/api';
+export const API = import.meta.env.VITE_API_URL ?? 'http://localhost:8000/api';
